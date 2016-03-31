2449e76f8b1818f8d47555efbb91ba0a0d2e6757
ben <741763560@qq.com>
2016-03-31 23:11:48 +0800

    xml解析

--- attendanceManagement_test/attendanceManagement_test/MainWindow.xaml.cs
+++ attendanceManagement_test/attendanceManagement_test/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.IO;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -13,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using attendanceManagement.NET;
+using attendanceManagement.XML;
 
 namespace attendanceManagement
 {
@@ -21,20 +23,64 @@ namespace attendanceManagement
     /// </summary>
     public partial class MainWindow : Window
     {
+        LinkedList<Course> coursesInfo = new LinkedList<Course>();
+
+
         public MainWindow()
         {
 
-            //DownLoad download = new DownLoad();
-            //download.getclasslist("12344321");
-            //download.getstulist("1234", "123456");
-            UpLoad upload = new UpLoad();
-            upload.checkin_file("1234","20160328","123456");
-            upload.checkin_one_stu("1234", "20160330", "123456", "1", "08:00:00", "10:00:00","123456");
-
             InitializeComponent();
+            openCourses();
+            foreach (Course course in coursesInfo)
+            {
+                TreeViewItem item = new TreeViewItem();
+                item.Header = course.get_lesson_name();
+                LinkedList<CourseDate> dates = course.get_dates();
+                foreach (CourseDate date in dates)
+                {
+                    TreeViewItem item2 = new TreeViewItem();
+                    item2.Header = date.toString();
+                    item2.MouseDoubleClick += test;
+                    item.Items.Add(item2);
+
+                }
+                treeView.Items.Add(item);
+            }
         }
 
-        private void dataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        void test(object sender, RoutedEventArgs e)
+        {
+            e.Handled = true;
+            TreeViewItem item = (TreeViewItem)sender;
+            item.Header = "haha";
+
+        }
+
+        void openCourses()  //打开courses的文件夹 读取文件信息
+        {
+            try
+            {
+                DirectoryInfo courses = new DirectoryInfo(@"courses");
+                FileSystemInfo[] list = courses.GetFileSystemInfos();
+                for (int i = 0; i < list.Length; i++)
+                {
+                    if (list[i].Extension.Equals(".xml"))
+                    {
+                        ZXmlDocument doc = new ZXmlDocument("courses\\" + list[i].Name);
+                        Course course = doc.getCourse();
+                        coursesInfo.AddFirst(course);
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                return;
+            }
+        }
+
+    
+
+    private void dataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
