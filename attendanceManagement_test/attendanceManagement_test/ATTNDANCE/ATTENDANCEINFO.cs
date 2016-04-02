30e78c0a46eaad7e9c31f81892e611a784acf0d7
BEN <741763560@qq.com>
2016-04-02 22:37:26 +0800

    搜索蓝牙设备，检测是否到课

---
+++ attendanceManagement_test/attendanceManagement_test/ATTNDANCE/ATTENDANCEINFO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace attendanceManagement.ATTENDANCE
+{
+    class ATTENDANCEINFO
+    {
+        public string name { get; set; }
+        public string college { get; set; }
+        public string major { get; set; }
+        public string sclass { get; set; }
+        public string checkattendace { get; set; }
+    }
+}
